fe96626fab69a3dda499f0b30a122e5fc17a7fe4
jjwarren44 <jjwarren44@gmail.com>
2021-10-22 18:10:42 -0400

    have kyber and uniswap prices coming in and normalized

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -1,4 +1,84 @@
+require("dotenv").config();
 const Web3 = require("web3");
+const { ChainId, Fetcher, TokenAmount } = require("@uniswap/sdk");
+const abis = require("./abis");
+const { mainnet: addresses } = require("./addresses");
+
 const web3 = new Web3(
-    new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/bce3144f41d24f1fb978095218f69cf7")
+    new Web3.providers.WebsocketProvider(process.env.INFURA_URL)
 );
+
+const kyber = new web3.eth.Contract(
+    abis.kyber.kyberNetworkProxy,
+    addresses.kyber.kyberNetworkProxy
+);
+
+const AMOUNT_ETH = 5;
+const RECENT_ETH_PRICE = 4000; // TODO: use API to regular update ETH price
+const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString());
+const AMOUNT_DAI_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_ETH_PRICE).toString());
+
+const init = async() => {
+    const [dai, weth] = await Promise.all(
+        [addresses.tokens.dai, addresses.tokens.weth].map(tokenAddress => (
+            Fetcher.fetchTokenData(
+                ChainId.MAINNET,
+                tokenAddress
+            )
+        ))
+    );
+
+    const daiWeth = await Fetcher.fetchPairData(
+        dai, weth
+    );
+
+    web3.eth.subscribe("newBlockHeaders")
+        .on("data", async block => {
+            console.log(`New block received. Block # ${block.number}`);
+
+            const kyberResults = await Promise.all([
+                kyber
+                .methods
+                .getExpectedRate(
+                    addresses.tokens.dai,
+                    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // ETH address for Kyber
+                    AMOUNT_DAI_WEI
+                )
+                .call(),
+
+                kyber
+                .methods
+                .getExpectedRate(
+                    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // ETH address for Kyber
+                    addresses.tokens.dai,
+                    AMOUNT_ETH_WEI
+                )
+                .call(),
+            ]);
+            const kyberRates = {
+                buy: parseFloat(1 / (kyberResults[0].expectedRate / (10 ** 18))),
+                sell: parseFloat(kyberResults[1].expectedRate / (10 ** 18))
+            };
+
+            console.log("Kyber ETH/DAI");
+            console.log(kyberRates);
+
+            const uniswapResults = await Promise.all([
+                daiWeth.getOutputAmount(new TokenAmount(dai, AMOUNT_DAI_WEI)),
+                daiWeth.getOutputAmount(new TokenAmount(weth, AMOUNT_ETH_WEI))
+            ]);
+            const uniswapRates = {
+                buy: parseFloat(AMOUNT_DAI_WEI / (uniswapResults[0][0].toExact() * 10 ** 18)),
+                sell: parseFloat(uniswapResults[1][0].toExact() / AMOUNT_ETH)
+            };
+
+            console.log("Uniswap ETH/DAI");
+            console.log(uniswapRates);
+
+        })
+        .on("error", error => {
+            console.log(error);
+        });
+}
+
+init();
